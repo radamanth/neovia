f2e53c559ecb4fff1e5d994e15e0f8d116aeccf9
Sylvain Chabert <sylvain.chabert@gmail.com>
2013-09-16 21:46:21 +0200

    Ajouts logos Déplacement caroussel logos Suppression menu equipe Retour sur les membres dans la section "en savoir plus" Augmentation police de base

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,4 +3,29 @@ jQuery(document).ready(function($) {
     //to top
     $().UItoTop({ easingType: 'easeOutQuart' });
 
+    (function() {
+
+        var $menu = $('nav'),
+            optionsList = '';
+
+        $menu.find('li').each(function() {
+            var $this   = $(this),
+                $anchor = $this.children('a'),
+                selected  = $this.hasClass('active') ? 'selected' : '';
+                indent  = '';
+
+
+            $(".nav li").parent().addClass("bold");
+
+
+            optionsList += '<option value="' + $anchor.attr('href') + '" ' + selected + '>' + indent + ' ' + $anchor.text() + '</option>';
+        }).end()
+            .after('<select class="selectmenu">' + optionsList + '</select>');
+
+        $('select.selectmenu').on('change', function() {
+            window.location = $(this).val();
+        });
+
+    })();
+
 });
